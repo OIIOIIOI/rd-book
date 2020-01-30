225d93f8102b8c04c9f02a84bfc40d83fb6e824f
Sylvere Armange <sylvere@mano.fr>
2020-01-30 16:30:19 +0100

    marquee test

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,16 +1,26 @@
 <template>
 	<div>
+		<marquee-text :duration="10">Prochain match : les <span class="font-bold">Divines Machines</span> vs les Bomb-Hard</marquee-text>
 		<h2>ACCUEIL</h2>
-		<a href="#">TEST</a>
 	</div>
 </template>
 
 <script>
+	import MarqueeText from 'vue-marquee-text-component';
+	
     export default {
-        name: "Home"
+        name: "Home",
+	    components: {
+            MarqueeText,
+	    },
     }
 </script>
 
 <style scoped>
-
+	.marquee-text-wrap {
+		@apply py-3 bg-black text-pink-200;
+	}
+	.marquee-text-text {
+		@apply px-6;
+	}
 </style>
